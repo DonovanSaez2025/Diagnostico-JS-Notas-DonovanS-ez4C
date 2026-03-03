5aa5c13c899cc9b9e85d53a1a5a1383b6f7affc9
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-03-03 12:26:46 -0300

    g
    g

---
+++ script.js
@@ -0,0 +1,21 @@
+const estudiantes = ["Ana", "Pedro", "Lucía", "Carlos", "Sofía"];
+let registroNotas = [];
+
+function notasEstudiantes() {
+    for (let i = 0; i < estudiantes.length; i++) {
+        let notaAlumno = parseInt(prompt(`Ingrese la nota final de ${estudiantes[i]}:`));
+        
+        if (notaAlumno >= 4 && notaAlumno < 8) {
+            alert(`${estudiantes[i]} aprobada/o`);
+        } else if (notaAlumno < 4 && notaAlumno > 0) {
+            alert(`${estudiantes[i]} reprobada/o`);
+        } else {
+            alert("Ingrese valores válidos.");
+            break;
+        };
+        registroNotas.push(notaAlumno);
+    };
+
+    alert(`Resumen del semestre: ${registroNotas}`);
+    registroNotas = [];
+};
